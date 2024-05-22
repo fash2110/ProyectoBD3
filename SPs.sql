8945bb68df856ef85260b0701f5a8f36e0438f23
fash2110 <Fash2110@gmail.com>
2024-05-21 19:54:58 -0600

    CargarXMLs finales

--- SPs.sql
+++ SPs.sql
@@ -55,6 +55,8 @@ GO;
 ALTER PROCEDURE [dbo].[CargarXmlDatos] (@XMLData XML)
 AS
 BEGIN
+	SET NOCOUNT ON
+
 	--Clientes
     INSERT INTO [dbo].[Clientes] (Nombre, Identificacion)
     SELECT 
@@ -63,36 +65,53 @@ BEGIN
     FROM @XMLData.nodes('/Operaciones/FechaOperacion/ClienteNuevo') AS t(c);
 
 	--Contratos
-	INSERT INTO [dbo].[Contrato] (Numero, idCliente, DocIdCliente, idTarifa)
+	INSERT INTO [dbo].[Contrato] (Numero, idCliente, DocIdCliente, idTarifa, Fecha)
 	SELECT
 		c.value('@Numero', 'VARCHAR(64)'),
 		Clientes.id,
 		c.value('@DocIdCliente', 'INT'),
-		c.value('@TipoTarifa', 'INT')
-	FROM @XMLData.nodes('/Operaciones/FechaOperacion/NuevoContrato') as t(c)
+		c.value('@TipoTarifa', 'INT'),
+		CAST(d.value('@fecha', 'NVARCHAR(32)') AS DATE)
+	FROM @XMLData.nodes('/Operaciones/FechaOperacion') as r(d)
+		CROSS APPLY d.nodes('NuevoContrato') AS t(c)
 		left join Clientes on c.value('@DocIdCliente', 'INT') = Clientes.Identificacion
-
+	
 	--Llamadas
-	INSERT INTO [dbo].[LlamadaTelefonica] (NumeroA, NumeroDe, Inicio, Final, idDetalles)
+	INSERT INTO [dbo].[LlamadaTelefonica] (NumeroA, NumeroDe, Inicio, Final)
 	SELECT
 		c.value('@NumeroA', 'VARCHAR(64)'),
 		c.value('@NumeroDe', 'VARCHAR(64)'),
 		CAST(c.value('@Inicio', 'NVARCHAR(32)') AS DATETIME),
-		CAST(c.value('@Final', 'NVARCHAR(32)') AS DATETIME),
-		NULL
+		CAST(c.value('@Final', 'NVARCHAR(32)') AS DATETIME)
 	FROM @XMLData.nodes('/Operaciones/FechaOperacion/LlamadaTelefonica') as t(c)
 
 	--UsoDatos
-	INSERT INTO [dbo].[UsoDatos] (Numero, QGigas, idDetalles)
+	INSERT INTO [dbo].[UsoDatos] (Numero, QGigas)
 	SELECT
 		c.value('@Numero', 'VARCHAR(64)'),
-		c.value('@QGigas','FLOAT'),
-		NULL
+		c.value('@QGigas','FLOAT')
 	FROM @XMLData.nodes('/Operaciones/FechaOperacion/UsoDatos') AS t(c)
 
 	--TODO: generar facturas para poder pagarlas
+	INSERT INTO Factura (Fecha, FechaPago, idContrato, TotalAntesIVA, TotalConIVA, TotalConPendiente, Pagado)
+SELECT
+	CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE),
+	DATEADD(MONTH, 1,  CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE)),
+	Contrato.id,
+	0,
+	0,
+	0,
+	0
+	FROM @XMLData.nodes('/Operaciones/FechaOperacion') as t(c), Contrato
+	WHERE
+		DAY(CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE)) =
+		CASE 
+		WHEN DAY(Contrato.Fecha) <= DAY(EOMONTH(CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE))) THEN DAY(Contrato.Fecha)
+		ELSE DAY(EOMONTH(CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE)))
+		END
+		AND MONTH(Contrato.Fecha) <= MONTH(CAST(c.value('@fecha', 'VARCHAR(32)') AS DATE))
 
-	--TipoRelacionesFamiliar
+	--RelacionesFamiliar
 	INSERT INTO [dbo].[RelacionFamiliar] (ClienteDe, ClienteA, idTipoRelacionesFamiliar)
 	SELECT
 		Cde.id,
@@ -101,20 +120,25 @@ BEGIN
 	FROM @XMLData.nodes('/Operaciones/FechaOperacion/RelacionFamiliar') AS t(c)
 		left join Clientes Cde on c.value('@DocIdDe', 'INT') = Cde.Identificacion
 		left join CLientes Ca on c.value('@DocIdA', 'INT') = Ca.Identificacion
+
+	SET NOCOUNT OFF
 END;
 GO;
 -- ####################################################################
-DELETE FROM Clientes
+DELETE FROM Detalles
+DELETE FROM Factura
 DELETE FROM Contrato
+DELETE FROM RelacionFamiliar
+DELETE FROM Clientes
 DELETE FROM LlamadaTelefonica
 DELETE FROM UsoDatos
-DELETE FROM RelacionFamiliar
 
 DBCC CHECKIDENT (Clientes, RESEED, 0);
 DBCC CHECKIDENT (Contrato, RESEED, 0);
 DBCC CHECKIDENT (LlamadaTelefonica, RESEED, 0);
 DBCC CHECKIDENT (UsoDatos, RESEED, 0);
 DBCC CHECKIDENT (RelacionFamiliar, RESEED, 0);
+DBCC CHECKIDENT (Factura, RESEED, 0);
 
 
 DECLARE @datos XML
@@ -125,3 +149,5 @@ FROM OPENROWSET(BULK 'C:\Users\fash2\OneDrive\Documentos\GitHub\ProyectoBD3\oper
 EXEC CargarXmlDatos @datos
 -- ####################################################################
 GO;
+
+SELECT * FROM Contrato
